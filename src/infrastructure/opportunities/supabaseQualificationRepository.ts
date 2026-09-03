d71116e3afdb3d2b8ecf6bc8514742650b6cf5b6
4ldwin88 <thinhjay.nguyen@gmail.com>
2026-09-02 22:09:15 -0400

    Remove obsolete Opportunity compatibility repository

--- src/infrastructure/opportunities/supabaseQualificationRepository.ts
+++
@@ -1,7 +0,0 @@
-export {
-  supabaseQualificationRepository,
-  type QualificationArea,
-  type QualificationAssessment,
-  type QualificationDecision,
-  type QualificationFinding,
-} from '../qualification/supabaseQualificationRepository';
