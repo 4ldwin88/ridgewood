b407db470d69050e791524fea870ebdd96c29705
4ldwin88 <thinhjay.nguyen@gmail.com>
2026-09-02 20:48:51 -0400

    Remove obsolete Opportunity repository port

--- src/application/ports/opportunityRepository.ts
+++
@@ -1,8 +0,0 @@
-import type { Opportunity } from '../../domain/opportunity/opportunity';
-
-export interface OpportunityRepository {
-  create(opportunity: Opportunity): Promise<Opportunity>;
-  getById(id: string): Promise<Opportunity | null>;
-  save(opportunity: Opportunity): Promise<Opportunity>;
-  list(): Promise<Opportunity[]>;
-}
